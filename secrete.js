26b6b8308bc0f62da9c536337ba8ba258f7e7015
hrs234 <itsharris892@gmail.com>
2019-09-28 13:54:22 +0700

    second changes

--- secrete.js
+++ secrete.js
@@ -1,15 +1,22 @@
-let a = "UKUB UK AMIL";
-let convert = a.split(" ");
-let res = [];
-let temp = [];
-for (let init = 0; convert.length - 1 >= init; init++) {
-  for (let binit = convert[init].length - 1; binit >= 0; binit--) {
-    temp.push(convert[init][binit]);
-    if (binit === 0) {
-      res.push(temp.join(""));
-      temp = [];
+let secrete = a => {
+  if (a.length >= 1000) {
+    return "ERROR: karakter melebihi dari 1000 karakter";
+  }
+  let convert = a.split(" ");
+  let res = [];
+  let temp = [];
+  for (let init = 0; convert.length - 1 >= init; init++) {
+    for (let binit = convert[init].length - 1; binit >= 0; binit--) {
+      temp.push(convert[init][binit]);
+      if (binit === 0) {
+        res.push(temp.join(""));
+        temp = [];
+      }
     }
   }
-}
+  return res.join(" ");
+};
 
-console.log(res.join(" "));
+console.log(secrete("italem irad irigayaj"));
+console.log(secrete("iadab itsap ulalreb"));
+console.log(secrete("nalub kusutret gnalali"));
